bae15e1ba2b2a5b0977d05b4cc4f53f3d72195d5
thebrianbn <thebrianbn@gmail.com>
2019-03-16 14:58:49 -0400

    initial models

--- smart_jobs/models.py
+++ smart_jobs/models.py
@@ -1,3 +1,49 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class AppUser(User):
+
+    position_preference = models.CharField(max_length=50, null=True)
+    education = models.CharField(max_length=50, null=True)
+    image = models.ImageField(null=True)
+
+
+class Resumes(models.Model):
+
+    resume_name = models.CharField(max_length=25)
+    resume_file = models.FileField()
+    username = models.ForeignKey(AppUser, on_delete=models.CASCADE)
+
+    class Meta:
+
+        unique_together = (("username", "resume_name"),)
+
+
+class JobApplications(models.Model):
+
+    job_title = models.CharField(max_length=25, primary_key=True)
+    job_description = models.TextField()
+    min_education = models.CharField(max_length=50, null=True)
+
+
+class UserApplications(models.Model):
+
+    username = models.ForeignKey(AppUser, on_delete=models.CASCADE)
+    job_title = models.ForeignKey(JobApplications, on_delete=models.DO_NOTHING)
+    resume_name = models.ForeignKey(Resumes, on_delete=models.DO_NOTHING)
+    status = models.CharField(max_length=25, null=True)
+
+    class Meta:
+
+        unique_together = (("username", "job_title"),)
+
+
+class Recommendations(models.Model):
+
+    username = models.ForeignKey(AppUser, on_delete=models.CASCADE)
+    job_title = models.ForeignKey(JobApplications, on_delete=models.CASCADE)
+
+    class Meta:
+
+        unique_together = (("username", "job_title"),)
