161fbc3b3c173a44fe3353253d54bb47b391fd7c
thebrianbn <thebrianbn@gmail.com>
2019-03-16 17:58:11 -0400

    models in admin

--- smart_jobs/admin.py
+++ smart_jobs/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from smart_jobs.models import AppUser, Resumes, JobApplications, UserApplications, Recommendations
 
 # Register your models here.
+admin.site.register(AppUser)
+admin.site.register(Resumes)
+admin.site.register(JobApplications)
+admin.site.register(UserApplications)
+admin.site.register(Recommendations)
