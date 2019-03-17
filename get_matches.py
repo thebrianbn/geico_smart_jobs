310a2fdc33eef77053abfc36b24e6a9b65b5ad32
Kyle Bradley <kj.bradley987@gmail.com>
2019-03-16 23:19:28 -0400

    Resume Matching
    initial commit

---
+++ get_matches.py
@@ -0,0 +1,39 @@
+import ast
+import json
+from nltk.corpus import stopwords
+from docx import Document
+from parse_docx import parse_docx
+import os
+from heapq import nlargest
+
+# Set up django environment
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "geico_smart_jobs.settings")
+import django
+django.setup()
+from smart_jobs.models import JobApplications
+
+def get_matches(path):
+    doc = path
+
+    new_tokens = parse_docx(doc)
+
+    jobs = JobApplications.objects.all()
+
+
+    match_dict = {}
+    for job in jobs:
+        tmp = ''.join(c for c in job.job_description if c not in "[]',")
+
+        match_count = 0
+        for word in tmp.split(" "):
+
+            if word in new_tokens:
+                match_count+=1
+        match_dict[job.job_title] = match_count
+    five = nlargest(5, match_dict, key=match_dict.get)
+    print(five)
+if __name__ == "__main__":
+    get_matches('software-engineer-midlevel')
+
+
+
